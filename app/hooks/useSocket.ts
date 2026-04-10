c59923ab6dd9732fb0889af823bbebb9d86a697f
mdYeasinIslam <hyeasinislam@gmail.com>
2026-04-10 20:41:58 +0600

    successfully connect socket

--- app/hooks/useSocket.ts
+++ app/hooks/useSocket.ts
@@ -2,20 +2,22 @@
 import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 
-const SOCKET_URL = process.env.VITE_SOCKET_URL;
-
-const useSocket = () => {
+const SOCKET_URL = process.env.NEXT_PUBLIC_SOCKET_URL;
+export const useSocket = () => {
   const socketRef = useRef<Socket | null>(null);
+  const [socket, setSocket] = useState<Socket | null>(null);
   const [connected, setConnected] = useState(false);
 
   useEffect(() => {
     //create socket connection
-    socketRef.current = io(SOCKET_URL, {
+    const socketInstance = io(SOCKET_URL, {
       transports: ["websocket", "polling"],
     });
+    socketRef.current = socketInstance;
+    setSocket(socketInstance);
 
     //connection event
-    socketRef.current.on("connection", () => {
+    socketRef.current.on("connect", () => {
       setConnected(true);
       console.log("socket is connected to server", socketRef.current?.id);
     });
@@ -26,21 +28,18 @@ const useSocket = () => {
     });
 
     //welcome message (initial try)
-    socketRef.current.on("connection", (data) => {
-      console.log(data);
+    socketRef.current.on("connected", (data) => {
+      console.log("socket connected", data);
     });
 
     return () => {
       if (socketRef.current) {
-        socketRef.current?.disconnect();
+        socketInstance?.disconnect();
       }
     };
   }, []);
-    const data :{socket:Socket|null,connected:boolean} = {
-    socket: socketRef.current,
+  return {
+    socket,
     connected,
-  }
-  return data;
+  };
 };
-
-export default useSocket;
--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
+'use client'
+import { useSocket } from "./hooks/useSocket";
 
 export default function Home() {
+const {socket,connected} = useSocket()
   return (
     <div>
-      
-   </div>
+      {`Socket is connected: ${connected}`}
+      <h1> {socket?.id}</h1>
+    </div>
   );
 }
