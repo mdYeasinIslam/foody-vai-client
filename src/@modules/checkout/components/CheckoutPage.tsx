c9821091654de9d77092864a55b2de952b7c8cc3
mdYeasinIslam <hyeasinislam@gmail.com>
2026-06-06 14:23:20 +0600

    fix delivery date field, add offers page and remove coupon section

--- src/@modules/checkout/components/CheckoutPage.tsx
+++ src/@modules/checkout/components/CheckoutPage.tsx
@@ -4,18 +4,17 @@ import { calculateTotal } from "@/src/@libs/utils/helperFunction";
 import { DatePicker } from "antd";
 import dayjs from "dayjs";
 import Image from "next/image";
+import Link from "next/link";
 import React, { useState } from "react";
-import { IoIosArrowDown, IoIosArrowUp } from "react-icons/io";
 import { ClassNameValue } from "tailwind-merge";
 import { ICartItem } from "../../cart/libs/interfaces";
 import { Delivery_Charge } from "../libs/enums";
 import DeliveryAddressForm from "./DeliveryAddressForm";
-import Link from "next/link";
+import BaseButton from "@/src/@base/components/BaseButton";
 interface IProps {
   className?: ClassNameValue;
 }
 const CheckoutPage: React.FC<IProps> = () => {
-  const [showCoupon, setShowCoupon] = useState(true);
   const [paymentMethod, setPaymentMethod] = useState("online");
 
   const [cart] = useGlobalState<ICartItem[]>({
@@ -36,65 +35,24 @@ const CheckoutPage: React.FC<IProps> = () => {
             <DeliveryAddressForm />
 
             {/* Coupon Section */}
-            <div className="border border-(--primary-color-500) rounded-lg ">
-              <div className="flex justify-between items-center bg-(--primary-color-600) px-4 py-2">
-                <h3 className="text-base font-semibold ">Coupon and Others</h3>
-                <div>
-                  <button
-                    onClick={() => setShowCoupon(!showCoupon)}
-                    className="flex items-center gap-1 text-sm font-semibold cursor-pointer"
-                  >
-                    <span>{showCoupon ? "Hide" : "Show"}</span>
-                    {showCoupon ? (
-                      <IoIosArrowUp className="w-4 h-4" />
-                    ) : (
-                      <IoIosArrowDown className="w-4 h-4" />
-                    )}
-                  </button>
-                </div>
-              </div>
-              {showCoupon && (
-                <div className="space-y-4 p-2 lg:p-6">
-                  <div className="flex gap-2">
-                    <input
-                      type="text"
-                      placeholder="Have a coupon code?"
-                      className="flex-1 px-4 py-1 md:py-2 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-(--primary-color-800)"
-                    />
-                    <button className="bg-(--primary-color-700) hover:bg-(--primary-color-800) text-white max-sm:text-xs px-2 py-1 md:px-6 md:py-2 rounded-lg font-medium cursor-pointer">
-                      Apply
-                    </button>
-                  </div>
-                  {/* <div className="flex items-center justify-between  px-4 py-1">
-                  <label className="flex items-center gap-3 cursor-pointer">
-                    <input type="checkbox" className="w-5 h-5" />
-                    <span className="text-gray-700">Points</span>
-                  </label>
-                  <span className="text-green-600 text-sm font-medium">
-                    Available: 0
-                  </span>
-                </div> */}
-                </div>
-              )}
-            </div>
+            {/* <CouponSection /> */}
 
             {/* Delivery Date */}
             <div className="border border-(--primary-color-500) rounded-lg ">
               <div className="bg-(--primary-color-600) px-4 py-2">
                 <h3 className="text-base font-semibold">Delivery Date</h3>
               </div>
-              {/* <input
-                type="text"
-                value="Thu, Apr 23, 2026"
-                className="w-full px-4 py-2 border border-(--primary-color-500) rounded-lg focus:outline-none focus:ring-2 focus:ring-(--primary-color-600)"
-                readOnly
-              /> */}
               <DatePicker
                 size={"large"}
                 placement="bottomLeft"
                 className="w-full border-(--primary-color-600)!"
                 suffixIcon={null}
-                defaultPickerValue={dayjs()}
+                defaultValue={dayjs().add(1, "day")}
+                defaultPickerValue={dayjs().add(1, "day")}
+                disabledDate={(current) =>
+                  current && current < dayjs().endOf("day")
+                }
+                format="Do MMM, YYYY"
               />
             </div>
 
@@ -233,10 +191,10 @@ const CheckoutPage: React.FC<IProps> = () => {
               </div>
 
               {/* Order Button */}
-              <button className="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-3 rounded-lg transition">
+              {/* <button className="w-full bg-green-600 hover:bg-green-700 text-white font-semibold py-3 rounded-lg transition">
                 Order Now
-              </button>
-
+              </button> */}
+              <BaseButton content="Order Now" className=""/>
               {/* Terms */}
               <p className="text-gray-600 text-xs mt-4">
                 By placing your order, you agree to be bound by the Khaas Food
--- src/@modules/checkout/components/DeliveryAddressForm.tsx
+++ src/@modules/checkout/components/DeliveryAddressForm.tsx
@@ -14,8 +14,7 @@ const DeliveryAddressForm = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const [form] = Form.useForm();
   const [districtId, setDistrictId] = useState<number | null>(null);
-  const { districtsData, areasData, } =
-    useDistrictAndArea(districtId);
+  const { districtsData, areasData } = useDistrictAndArea(districtId);
   const { addressData, addressDataLoading, createCustomerAddressMutate } =
     useAddressState(messageApi);
 
@@ -58,7 +57,7 @@ const DeliveryAddressForm = () => {
           <h3 className="text-base font-semibold ">Delivery address</h3>
           <button
             onClick={handleAddNew}
-            className=" text-sm font-semibold cursor-pointer border hover:border-(--primary-color-900) border-(--primary-color-700) rounded-sm px-2"
+            className="btn-primary text-sm font-semibold cursor-pointer border hover:border-(--primary-color-900) border-(--primary-color-700) rounded-sm px-2! py-0.5!"
           >
             + Add new
           </button>
@@ -102,92 +101,6 @@ const DeliveryAddressForm = () => {
             areasData={areasData}
           />
         </BaseForm>
-        {/* <Form form={form} layout="vertical" onFinish={handleSubmit}>
-          <div className="grid grid-cols-2 gap-4">
-            <Form.Item
-              label="District"
-              name="districtId"
-              rules={[{ required: true, message: "Please select district" }]}
-            >
-              <Select
-                placeholder="Type or select district"
-                options={districtsData?.map((district) => ({
-                  label: district?.name,
-                  value: district.id,
-                }))}
-                onChange={(e) => setDistrictId(e)}
-                loading={isLoading && isPending}
-              />
-            </Form.Item>
-
-            <Form.Item
-              label="Area"
-              name="areaId"
-              rules={[{ required: true, message: "Please select area" }]}
-            >
-              <Select
-                placeholder="Select area first"
-                options={
-                  districtId
-                    ? areasData?.map((area) => ({
-                        label: area.name,
-                        value: area.id,
-                      }))
-                    : []
-                }
-                disabled={!districtId}
-              />
-            </Form.Item>
-          </div>
-
-          <div className="grid grid-cols-2 gap-4">
-            <Form.Item
-              label="Contact Name"
-              name="contactName"
-              rules={[{ required: true, message: "Please enter contact name" }]}
-            >
-              <Input placeholder="Enter contact person name" />
-            </Form.Item>
-
-            <Form.Item
-              label="Phone"
-              name="phone"
-              rules={[{ required: true, message: "Please enter phone number" }]}
-            >
-              <Input placeholder="01XXXXXXXXX" />
-            </Form.Item>
-          </div>
-
-          <Form.Item
-            label="Address Name"
-            name="addressName"
-            rules={[{ required: true, message: "Please select address name" }]}
-          >
-            <Select
-              placeholder="Select address type"
-              options={[
-                { label: "Home", value: "home" },
-                { label: "Office", value: "office" },
-                { label: "Other", value: "other" },
-              ]}
-            />
-          </Form.Item>
-
-          <Form.Item
-            label="Address"
-            name="address"
-            rules={[{ required: true, message: "Please enter address" }]}
-          >
-            <Input.TextArea
-              placeholder="i.e. House 12, Road 3, Block E, Uttara"
-              rows={4}
-            />
-          </Form.Item>
-
-          <Form.Item>
-            <BaseButton content="+ Add Address" />
-          </Form.Item>
-        </Form> */}
       </BaseModal>
     </>
   );
