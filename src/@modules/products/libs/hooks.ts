ade7598fae7075e8c1c25dd4844c246fc68c70d2
mdYeasinIslam <hyeasinislam@gmail.com>
2026-07-09 20:30:15 +0600

    add create, update and delete hooks for product ---admin pannel

--- src/@modules/products/libs/hooks.ts
+++ src/@modules/products/libs/hooks.ts
@@ -1,13 +1,13 @@
 import { QueryConfig } from "@/src/@libs/config/react-query";
-import { useQuery } from "@tanstack/react-query";
-import { IProductFilter } from "./interfaces";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import { IProductCreateAndUpdate, IProductFilter } from "./interfaces";
 import { ProductsService } from "./services";
 
 type IUseProducts = {
   options?: IProductFilter;
   config?: QueryConfig<typeof ProductsService.find>;
 };
-export const useProducts = ({ options, }: IUseProducts) => {
+export const useProducts = ({ options }: IUseProducts) => {
   return useQuery({
     queryKey: ["products"],
     queryFn: () => ProductsService.find(),
@@ -19,9 +19,56 @@ type IUseProduct = {
   config: QueryConfig<typeof ProductsService.findById>;
 };
 export const useProduct = ({ id, config }: IUseProduct) => {
-    return useQuery({
+  return useQuery({
     ...config,
     queryKey: ["product", id],
     queryFn: () => ProductsService.findById(id),
   });
-}
+};
+export const useCreateProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ProductsService.create,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["products"],
+      });
+    },
+  });
+};
+
+export const useUpdateProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ({
+      id,
+      payload,
+    }: {
+      id: string;
+      payload: Partial<IProductCreateAndUpdate>;
+    }) => ProductsService.update(id, payload),
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["products"],
+      });
+    },
+  });
+};
+
+export const useDeleteProduct = () => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: ProductsService.delete,
+
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: ["products"],
+      });
+    },
+  });
+};
