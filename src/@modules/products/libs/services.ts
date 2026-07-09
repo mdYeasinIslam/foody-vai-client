3d6d866b5921a57e0e4c37fa4a3f7de5219fa31f
mdYeasinIslam <hyeasinislam@gmail.com>
2026-07-09 20:28:26 +0600

    add services in product services

--- src/@modules/products/libs/services.ts
+++ src/@modules/products/libs/services.ts
@@ -1,7 +1,7 @@
 
 import { AxiosInstance } from "@/src/@libs/config/AxiosInstance";
 import axios from "axios";
-import { IProductFilter, IProductResponse, IProductsResponse } from "./interfaces";
+import { IProductCreateAndUpdate, IProductFilter, IProductResponse, IProductsResponse } from "./interfaces";
 
 const END_POINT: string = "products";
 
@@ -12,7 +12,7 @@ export const ProductsService = {
       const res = await AxiosInstance.post(END_POINT, payload);
       return Promise.resolve(res?.data);
     } catch (error) {
-      throw error
+      throw error;
     }
   },
   async find(query?: IProductFilter): Promise<IProductsResponse> {
@@ -30,5 +30,18 @@ export const ProductsService = {
     } catch (error) {
       throw error;
     }
+  },
+  update: async (
+    id: string,
+    payload: Partial<IProductCreateAndUpdate>,
+  ): Promise<IProductResponse> => {
+    const res = await AxiosInstance.put(`${END_POINT}/${id}`, payload);
+
+    return res.data;
+  },
+
+  delete: async (id: string): Promise<IProductResponse> => {
+    const res = await AxiosInstance.delete(`${END_POINT}/${id}`);
+    return res.data;
   },
 };
